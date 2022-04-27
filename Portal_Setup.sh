2ef8ba5c4f59b310993e39b399adf598e4965079
Charles Farris <charlesf@pandell.com>
2022-04-27 15:34:50 -0600

    Add authorization file

--- Portal_Setup.sh
+++ Portal_Setup.sh
@@ -7,4 +7,4 @@ sudo tar -xf /mnt/cdrom/ArcGIS_Server_1061/Portal_for_ArcGIS_Linux_1061_164055.t
 
 # install ArcGIS Server
 echo Run ArcGIS Server Setup...
-sh /home/cfarris/install/PortalForArcGIS/Setup -m silent -l Yes
+sh /home/cfarris/install/PortalForArcGIS/Setup -m silent -l Yes -a /mnt/cdrom/ArcGISGISServerAdvanced_ArcGISServer_1153083.prvc

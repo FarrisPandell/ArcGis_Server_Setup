a61cb9a6d8b57f7a0cab875734080376c25030b7
Charles Farris <charlesf@pandell.com>
2022-04-27 15:15:14 -0600

    Fix bad name

--- Portal_Setup.sh
+++ Portal_Setup.sh
@@ -7,4 +7,4 @@ sudo tar -xf /mnt/cdrom/ArcGIS_Server_1061/Portal_for_ArcGIS_Linux_1061_164055.t
 
 # install ArcGIS Server
 echo Run ArcGIS Server Setup...
-sh /home/cfarris/install/ArcGISPortal_Linux/Setup -m silent -l Yes
+sh /home/cfarris/install/PortalForArcGIS/Setup -m silent -l Yes

e14fa0e7558d1bae573f3974958d8cb344b2b96b
Charles Farris <charlesf@pandell.com>
2022-05-02 17:26:43 -0600

    Fix install paths

--- WebAdaptor_Setup.sh
+++ WebAdaptor_Setup.sh
@@ -3,8 +3,8 @@
 # unpack 
 echo Unpack Installation Media...
 sudo mkdir /home/cfarris/install
-sudo tar -xf /mnt/cdrom/ArcGIS_Server_1061/Web_Adaptor_Java_Linux_1061_164057.tar.gz -C /home/cfarris/install
+sudo tar -xf /mnt/cdrom/ArcGIS_Web_Adaptor_Java_Linux_109_177888.tar.gz -C /home/cfarris/install
 
 # install ArcGIS WebAdaptorc
 sh /home/cfarris/install/WebAdaptor/Setup -m silent -l Yes
-sudo cp /home/cfarris/arcgis/webadaptor10.6.1/java/arcgis.war /usr/share/tomcat/webapps/
+sudo cp /home/cfarris/arcgis/webadaptor10.9.0/java/arcgis.war /usr/share/tomcat/webapps/

238a22bf2386bb02f9878e77b0d8d9cf9f3562e6
Charles Farris <charlesf@pandell.com>
2022-05-06 14:19:27 -0600

    Add wget install

--- System_WebAdaptor_Setup.sh
+++ System_WebAdaptor_Setup.sh
@@ -7,6 +7,7 @@ sudo yum update -y
 # update installation tools
 echo Update Installation Tools
 sudo yum install nano -y
+sudo yum install wget -y
 
 # open ArcGIS Web Adaptor ports
 echo Open Ports...

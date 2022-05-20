b2f8c273e46368b1fd1d7aff1606dd3c49c2ffbf
Charles Farris <charlesf@pandell.com>
2022-05-20 07:19:02 -0600

    Add Java install
    - Fix copy path

--- Tomcat_Setup.sh
+++ Tomcat_Setup.sh
@@ -1,5 +1,7 @@
+# install Java
+sudo yum install java-1.8.0-openjdk-devel -y
+
 # unzip Tomcat
 cd /home/cfarris
 tar -xf /mnt/cdrom/apache-tomcat-9.0.63.tar.gz -C /home/cfarris
-mv mv apache-tomcat-9.0.63/ tomcat9
-
+mv apache-tomcat-9.0.63/ tomcat9

c39778ce846d36b835879f82c186c9d067954a8f
Charles Farris <charlesf@pandell.com>
2022-05-24 17:51:15 -0600

    Update port handling

--- tomcat_setup.sh
+++ tomcat_setup.sh
@@ -1,7 +1,11 @@
 # open Tomcat ports
 echo Opening Ports...
+sudo firewall-cmd --add-port=80/tcp --zone=public --permanent
+sudo firewall-cmd --add-port=443/tcp --zone=public --permanent
 sudo firewall-cmd --add-port=8080/tcp --zone=public --permanent
 sudo firewall-cmd --add-port=8443/tcp --zone=public --permanent
+firewall-cmd --add-forward-port=port=443:proto=tcp:toport=8443
+firewall-cmd --direct --add-rule ipv4 nat OUTPUT 0 -p tcp -o lo --dport 443 -j REDIRECT --to-ports 8443
 # reload firewall
 sudo firewall-cmd --reload
 

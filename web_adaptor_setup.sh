180b74c264f16901b4c369da6304198282901ec2
Charles Farris <charlesf@pandell.com>
2022-05-24 15:33:36 -0600

    Open ports
    - Add port forwarding
    - Add portal support

--- web_adaptor_setup.sh
+++ web_adaptor_setup.sh
@@ -2,8 +2,12 @@
 
 # open ArcGIS Web Adaptor ports
 echo Opening Ports...
-# sudo firewall-cmd --add-port=80/tcp --zone=public --permanent
-# sudo firewall-cmd --add-port=443/tcp --zone=public --permanent
+sudo firewall-cmd --add-port=80/tcp --zone=public --permanent
+sudo firewall-cmd --add-port=443/tcp --zone=public --permanent
+sudo firewall-cmd --add-port=8080/tcp --zone=public --permanent
+sudo firewall-cmd --add-port=8443/tcp --zone=public --permanent
+firewall-cmd --add-forward-port=port=443:proto=tcp:toport=8443
+firewall-cmd --direct --add-rule ipv4 nat OUTPUT 0 -p tcp -o lo --dport 443 -j REDIRECT --to-ports 8443
 # reload firewall
 sudo firewall-cmd --reload
 
@@ -14,4 +18,5 @@ sudo tar -xf /mnt/cdrom/ArcGIS_Web_Adaptor_Java_Linux_109_177888.tar.gz -C /home
 
 # install ArcGIS WebAdaptorc
 sh /home/cfarris/install/WebAdaptor/Setup -m silent -l Yes
-sudo cp /home/cfarris/arcgis/webadaptor10.9/java/arcgis.war /home/cfarris/tomcat9/webapps/
+sudo cp /home/cfarris/arcgis/webadaptor10.9/java/arcgis.war /home/cfarris/tomcat9/webapps/arcgis.war
+sudo cp /home/cfarris/arcgis/webadaptor10.9/java/arcgis.war /home/cfarris/tomcat9/webapps/portal.war

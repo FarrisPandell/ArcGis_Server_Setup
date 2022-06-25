eba6013f6203512d3a9727b5f4177e3eb19b4e79
Charles Farris <charlesf@pandell.com>
2022-06-25 14:03:46 -0600

    Add xrdp to centos71

--- centos71_setup.sh
+++ centos71_setup.sh
@@ -10,11 +10,19 @@ sudo yum install nano -y
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install unzip -y
 sudo yum install bind-utils -y
+sudo yum install epel-release -y
+sudo yum install xrdp -y
 
 # mount CD-ROM
 echo Mount CD-ROM...
 sudo mkdir /mnt/cdrom
 sudo mount /dev/sr0 /mnt/cdrom
+
+# start xrdp
+sudo firewall-cmd --add-port=3389/tcp --zone=public --permanent
+sudo firewall-cmd --reload
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
 
 # unzip certificates
 unzip /mnt/cdrom/certificates.zip -d /home/cfarris
